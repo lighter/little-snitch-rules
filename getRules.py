33ab98f5ace738d6b6230c6cb44f5b16dcb883fc
lighter <github@lighter.aleeas.com>
2023-05-04 18:50:13 +0800

    bug fix

--- getRules.py
+++ getRules.py
@@ -13,22 +13,22 @@ sources_dic = {
 
 
 ip_regex = r"(?:\d{1,3}\.){3}\d{1,3}"
-zero_ip_regex = r"0.0.0.0"
-url_regex = r"{ip}\s+(\S+)".format(ip=zero_ip_regex)
+zero_ip_regex = r"^0.0.0.0"
+url_regex = r"{ip}\s+(\S+)".format(ip=zero_ip_regex)    
 
 for file, url in sources_dic.items():
 
   r = requests.get(url)
   # 尋找匹配的資料,  0.0.0.0 之後的第一個空白字元之前的字串
   matches = re.findall(r"{url}".format(url=url_regex), r.text, re.MULTILINE)
-  
+
   with open(file, 'w') as f:
       f.write("{\n")
       f.write(f"  \"description\": \"Generated from {url}\",\n")
       f.write(f"  \"name\": \"{file}\",\n")
       f.write(f"  \"process\": \"any\",\n")
       f.write(f"  \"denied-remote-notes\": \"Retrieved on {iso_time} from list {file}\",\n")
-      f.write(f"  \"denied-remote-domains\": [")
+      f.write(f"  \"denied-remote-domains\": [\n")
 
       for i, match in enumerate(matches):
          # 這裡可以過濾掉不需要的 ip
@@ -37,7 +37,7 @@ for file, url in sources_dic.items():
             match = '"' + match + '"'
             if i != len(matches) - 1:
                match = match + ','
-            f.write(f"{match}")
+            f.write(f"    {match}\n")
 
-      f.write("]\n")
+      f.write("  ]\n")
       f.write("}")
